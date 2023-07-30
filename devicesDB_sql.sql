3eedf466ef040cd3dbe13f2e7302a93e07144501
Henri Toivonen <htoivonen@live.fi>
2023-07-30 12:31:54 +0300

    Created new devices table
    the devices table contains information about the devices and refers to the laptops table

--- devicesDB_sql.sql
+++ devicesDB_sql.sql
@@ -1,1 +1,8 @@
-ALTER TABLE devices RENAME TO laptops
+CREATE TABLE devices(
+device_id SERIAL PRIMARY KEY NOT NULL,
+	laptop_id INTEGER REFERENCES laptops(laptop_id) NOT NULL,
+	device_name VARCHAR(100) UNIQUE NOT NULL,
+	device_status VARCHAR(100) NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+	last_login TIMESTAMP
+);
