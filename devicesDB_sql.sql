a33b5899c6d181a4fb86716743868cba520bfb73
Henri Toivonen <htoivonen@live.fi>
2023-07-30 12:12:45 +0300

    Change column name

--- devicesDB_sql.sql
+++ devicesDB_sql.sql
@@ -1,12 +1,2 @@
-CREATE TABLE devices (
-device_id SERIAL PRIMARY KEY,
-	manufacturer VARCHAR(50),
-	model VARCHAR(50),
-	processor VARCHAR(50),
-	graphics_card VARCHAR(50),
-	memory VARCHAR(50),
-	screen_size VARCHAR(50),
-	resolution VARCHAR(50),
-	storage VARCHAR(50),
-	operating_system VARCHAR(50)
-)
+ALTER TABLE devices
+RENAME COLUMN device_id TO laptop_id
