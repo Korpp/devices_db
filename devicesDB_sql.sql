720fd219d187d5f5d4ed911f32b34ac641248789
Henri Toivonen <htoivonen@live.fi>
2023-07-30 12:33:37 +0300

    Backup

--- devicesDB_sql.sql
+++ devicesDB_sql.sql
@@ -1,8 +1,1 @@
-CREATE TABLE devices(
-device_id SERIAL PRIMARY KEY NOT NULL,
-	laptop_id INTEGER REFERENCES laptops(laptop_id) NOT NULL,
-	device_name VARCHAR(100) UNIQUE NOT NULL,
-	device_status VARCHAR(100) NOT NULL,
-	created_on TIMESTAMP NOT NULL,
-	last_login TIMESTAMP
-);
+SELECT * FROM DEVICES
