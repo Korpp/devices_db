0931f7b2b0a6d1c4d14550fa6f86747f4947021e
Henri Toivonen <htoivonen@live.fi>
2023-07-31 17:12:21 +0300

    Insert data manually into devices table

--- devicesDB_sql.sql
+++ devicesDB_sql.sql
@@ -1,1 +1,10 @@
-SELECT * FROM DEVICES
+INSERT INTO devices(laptop_id,device_name, device_status,created_on)
+VALUES(
+	4,'PC2CY0DKD', '1 = new', CURRENT_TIMESTAMP
+),
+(
+	4,'PC2CY0DKE', '1 = new', CURRENT_TIMESTAMP
+),
+(
+	5,'LTDJJ234DS', '3 = under maintenance', CURRENT_TIMESTAMP
+);
