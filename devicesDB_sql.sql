02622eba000881fb2aff0cba0f033141df380d6b
Henri Toivonen <htoivonen@live.fi>
2023-07-31 17:17:06 +0300

    Test Join
    testing with inner join on how to show device name from devices table and model name from laptops and getting results in data output

--- devicesDB_sql.sql
+++ devicesDB_sql.sql
@@ -1,10 +1,3 @@
-INSERT INTO devices(laptop_id,device_name, device_status,created_on)
-VALUES(
-	4,'PC2CY0DKD', '1 = new', CURRENT_TIMESTAMP
-),
-(
-	4,'PC2CY0DKE', '1 = new', CURRENT_TIMESTAMP
-),
-(
-	5,'LTDJJ234DS', '3 = under maintenance', CURRENT_TIMESTAMP
-);
+SELECT device_name, model FROM devices
+INNER JOIN laptops
+ON devices.laptop_id = laptops.laptop_id
